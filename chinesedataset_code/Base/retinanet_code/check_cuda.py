9316f621f4565ad96f6db1c76f9fb111357a86d2
kamaladhi <jeevakamal2005@gmail.com>
2026-04-13 09:21:50 +0530

    Deleting chinese dataset folder

--- chinesedataset_code/Base/retinanet_code/check_cuda.py
+++
@@ -1,9 +0,0 @@
-import torch
-print(f"PyTorch Version: {torch.__version__}")
-print(f"CUDA Available: {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"Current Device: {torch.cuda.current_device()}")
-    print(f"Device Name: {torch.cuda.get_device_name(0)}")
-    print(f"CUDA Capability: {torch.cuda.get_device_capability(0)}")
-else:
-    print("CUDA is NOT available. Check your drivers and environment.")
